4c89a3847f6b03347dd46d3e1b61416066cda43e
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-22 04:45:00 -0300

    Criado o metodo AdicionaFilme e a classe Filme

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,5 +1,18 @@
-﻿namespace FilmesApi.Controllers;
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class FilmeController
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
 {
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    
+    public void AdicionaFilme(Filme filme)
+    {
+        filmes.Add(filme);
+    }
 }
