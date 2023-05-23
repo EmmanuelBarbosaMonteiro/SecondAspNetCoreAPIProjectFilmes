81e3a9f9021b3a9b346db07c11868bb2fd0f6e31
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-23 04:07:31 -0300

    Adicionado a paginação com os métodos Skip() e Take()

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -21,9 +21,10 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes()
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip =0, 
+        [FromQuery] int take = 50)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
