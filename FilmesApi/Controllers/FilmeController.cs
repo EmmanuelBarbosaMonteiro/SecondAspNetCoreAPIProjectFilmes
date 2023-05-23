756462c18ebe9e433acc6bfbf5e9c2959764b1a9
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-23 03:48:45 -0300

    Adicionado a propriedade Id em Filme e o método RecuperaFilmePorId

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -9,10 +9,12 @@ public class FilmeController : ControllerBase
 {
 
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody]Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
@@ -22,5 +24,11 @@ public class FilmeController : ControllerBase
     public IEnumerable<Filme> RecuperaFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
