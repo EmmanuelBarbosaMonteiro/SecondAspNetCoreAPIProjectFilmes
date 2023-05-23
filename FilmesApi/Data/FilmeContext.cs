983a370aed8a4bbb6e90cb957cb968b363675c36
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-23 05:22:50 -0300

    Criada a pasta Data e a classe FilmeContext

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+            : base(opts)
+        {
+            
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
