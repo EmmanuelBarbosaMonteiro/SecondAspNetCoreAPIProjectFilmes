d49070708e328ccc6b12ffe4ae619c9756442243
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-05-30 06:53:09 -0300

    Criada a classe Sessao com DTOs, controladores e perfil AutoMapper para o conceito de Sessao

---
+++ FilmesApi/Data/Dtos/CreateSessaoDto.cs
@@ -0,0 +1,5 @@
+﻿namespace FilmesApi.Data.Dtos;
+
+public class CreateSessaoDto
+{
+}
---
+++ FilmesApi/Models/Sessao.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Sessao
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+}
