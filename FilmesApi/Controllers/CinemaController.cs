799bf538723d3906561e9ecfcd55b9a0f15be43f
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-06-02 06:04:44 -0300

    Adicionado filtro por EnderecoId para o método RecuperaCinemas

--- FilmesApi/Controllers/CinemaController.cs
+++ FilmesApi/Controllers/CinemaController.cs
@@ -3,6 +3,7 @@ using FilmesApi.Data;
 using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilmesApi.Controllers;
 
@@ -30,9 +31,18 @@ public class CinemaController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<ReadCinemaDto> RecuperaCinemas()
+    public IEnumerable<ReadCinemaDto> RecuperaCinemas([FromQuery]
+     int? enderecoId = null)
     {
-        return _mapper.Map<List<ReadCinemaDto>>(_context.Cinemas.ToList());
+        if(enderecoId == null)
+        {
+            return _mapper.Map<List<ReadCinemaDto>>
+                (_context.Cinemas.ToList());
+        }
+        return _mapper.Map<List<ReadCinemaDto>>
+            (_context.Cinemas.FromSqlRaw
+            ($"SELECT Id, Nome, EnderecoId FROM cinemas where cinemas.EnderecoId = {enderecoId}")
+            .ToList());
     }
 
     [HttpGet("{id}")]
